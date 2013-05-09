0c06c108a560c3fb30e9ed00facece6c77b4f65f
Kurt Lange <klange612@gmail.com>
2013-05-08 20:21:43 -0500

    Initial Commit

---
+++ Homepwner/HomepwnerAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HomepwnerAppDelegate.h
+//  Homepwner
+//
+//  Created by Kelley Lange on 5/8/13.
+//  Copyright (c) 2013 Kurt Lange. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomepwnerAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Homepwner/HomepwnerViewController.m
@@ -0,0 +1,29 @@
+//
+//  HomepwnerViewController.m
+//  Homepwner
+//
+//  Created by Kelley Lange on 5/8/13.
+//  Copyright (c) 2013 Kurt Lange. All rights reserved.
+//
+
+#import "HomepwnerViewController.h"
+
+@interface HomepwnerViewController ()
+
+@end
+
+@implementation HomepwnerViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
